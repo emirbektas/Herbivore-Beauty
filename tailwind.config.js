fe87b7df6f191bb294a666ed7e9a6bb1464eaaeb
emirbkts <emr.bkts@hotmail.com>
2023-03-31 15:31:37 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      guap: "#173f35",
+      whitey: "#f2f2f1",
+      gwite: "#ebeaef",
+      darky: "#1c1c1c",
+    },
+  },
+  plugins: [],
+};
