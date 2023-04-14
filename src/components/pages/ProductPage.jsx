3604eb3fa2e92c5f0a18c9e76ec9ffc4d50d8a02
emirbkts <emr.bkts@hotmail.com>
2023-04-15 00:35:36 +0300

    products styling

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -17,7 +17,7 @@ function ProductPage() {
         </div>
       </div>
       <div className="products p-5">
-        <div className="buttons flex justify-between p-2">
+        <div className="buttons flex justify-between mb-5 m-2">
           <button className="px-24 py-1 border border-guap flex hover:bg-guap hover:text-white">
             <BsFilter className="my-auto mr-2" />
             FILTER
@@ -27,12 +27,12 @@ function ProductPage() {
             Sort by
           </button>
         </div>
-        <div className="item-list grid grid-cols-2">
-          <div className="item flex flex-col p-2 relative">
-            <img src={Acid} className="bg-gwite cursor-pointer" />
-            <h3 className="font-bold text-lg">Header</h3>
-            <p className="text-sm">$10.99</p>
-            <button className="border border-guap text-center text-guap hover:opacity-50 hover:transition-opacity">
+        <div className="item-list grid grid-cols-2 ">
+          <div className="item flex flex-col p-2 relative bg-gwite rounded-lg m-2 hover:scale-105 transition-all">
+            <img src={Acid} className="bg-gwite cursor-pointer rounded-t-lg" />
+            <h3 className="font-bold text-lg mb-1">Header</h3>
+            <p className="text-sm mb-2">$10.99</p>
+            <button className="border border-guap text-center text-guap mb-2 hover:opacity-50 hover:transition-opacity">
               Add to bag
             </button>
             <span className="flex text-sm cursor-pointer">
@@ -43,16 +43,29 @@ function ProductPage() {
               <BsStarHalf className="my-auto mr-1" />
               60 Reviews
             </span>
-            <span className="absolute top-0 left-0 bg-guap p-1 ml-2 mt-2 text-white text-sm ">
+            <span className="absolute top-0 left-0 bg-guap p-1 text-white text-sm rounded-tl-lg">
               BESTSELLER
             </span>
           </div>
-          <div className="item flex flex-col p-2">
-            <img src={Acid} className="bg-gwite" />
-            <h3>Header</h3>
-            <p>Price</p>
-            <button className="flex">Add to bag</button>
-            <span>Reviews</span>
+
+          <div className="item flex flex-col p-2 relative bg-gwite rounded-lg m-2">
+            <img src={Acid} className="bg-gwite cursor-pointer rounded-t-lg" />
+            <h3 className="font-bold text-lg mb-1">Header</h3>
+            <p className="text-sm mb-2">$10.99</p>
+            <button className="border border-guap text-center text-guap mb-2 hover:opacity-50 hover:transition-opacity">
+              Add to bag
+            </button>
+            <span className="flex text-sm cursor-pointer">
+              <BsStarFill className="my-auto" />
+              <BsStarFill className="my-auto" />
+              <BsStarFill className="my-auto" />
+              <BsStarFill className="my-auto" />
+              <BsStarHalf className="my-auto mr-1" />
+              60 Reviews
+            </span>
+            <span className="absolute top-0 left-0 bg-guap p-1 text-white text-sm rounded-tl-lg">
+              BESTSELLER
+            </span>
           </div>
         </div>
       </div>
