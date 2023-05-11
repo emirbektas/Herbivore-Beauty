ff5057c3226433f35a24bc8acccb9e258ab12ee9
emirbkts <emr.bkts@hotmail.com>
2023-05-11 19:12:26 +0300

    responsive updates, slick done

--- src/components/MainPage/Bests.jsx
+++ src/components/MainPage/Bests.jsx
@@ -8,8 +8,10 @@ import Tonic from "../../images/other/tonic.jpg";
 import Bundle from "../../images/other/bundle.jpg";
 import { BsStarFill, BsStarHalf } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import useWindowSize from "../Hook/useWindowSize";
 
 function Bests() {
+  const size = useWindowSize();
   var settings = {
     dots: true,
     infinite: true,
@@ -19,19 +21,16 @@ function Bests() {
     responsive: [
       {
         breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
       },
     ],
   };
   return (
-    <div className="overflow-hidden bg-probg bg-half bg-no-repeat bg-center-top h-full w-full ">
+    <div className="overflow-hidden bg-probg bg-half bg-no-repeat bg-center-top h-full w-full">
       <div className="p-4">
         <h1 className="text-center font-bold text-2xl text-guap mt-2">
           OUR BEST SELLERS
         </h1>
-        <Slider {...settings} className="lg:w-4/5 mx-auto">
+        <Slider {...settings} className="lg:hidden">
           <div className="text-center p-10">
             <a href="">
               <div className="text-left">
@@ -54,13 +53,6 @@ function Bests() {
           <div className="text-center p-10 ">
             <a href="">
               <div className="text-left">
-                {/* <div class="absolute overflow-hidden w-56 h-56">
-                <div class="absolute left-0 top-0 h-16 w-16">
-                  <div class="absolute transform -rotate-45 bg-guap text-center text-white font-semibold py-1 left-[-34px] top-[32px] w-[170px]">
-                    Popular
-                  </div>
-                </div>
-              </div> */}
                 <img src={Sunscreen} className="rounded" />
                 <h1 className="font-bold mb-2">Sunscreen</h1>
 
@@ -124,6 +116,95 @@ function Bests() {
             </a>
           </div>
         </Slider>
+        {size.width > 1024 && (
+          <div className="grid grid-cols-4 p-10 gap-10">
+            <div className="text-center">
+              <a href="">
+                <div className="text-left">
+                  <img src={Cleanser} className="rounded " />
+                  <h1 className="font-bold mb-2">Cleanser</h1>
+                  <p className="text-guap italic text-sm">$15.99</p>
+                  <div className="reviews flex">
+                    <p className="text-sm flex my-auto text-yellow-400">
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarHalf />
+                    </p>
+                    <p className="text-sm ml-1 text-yellow-600">5 Reviews</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="text-center ">
+              <a href="">
+                <div className="text-left">
+                  <img src={Sunscreen} className="rounded" />
+                  <h1 className="font-bold mb-2">Sunscreen</h1>
+
+                  <p className="text-guap italic text-sm">$15.99</p>
+                  <div className="reviews flex">
+                    <p className="text-sm flex my-auto text-yellow-400">
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarHalf />
+                    </p>
+                    <p className="text-sm ml-1 text-yellow-600">5 Reviews</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="text-center ">
+              <a href="">
+                <div className="text-left">
+                  <img src={Tonic} className="rounded" />
+                  <h1 className="font-bold mb-2">Tonic</h1>
+                  <p className="text-guap italic text-sm">$15.99</p>
+                  <div className="reviews flex">
+                    <p className="text-sm flex my-auto text-yellow-400">
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarHalf />
+                    </p>
+                    <p className="text-sm ml-1 text-yellow-600">5 Reviews</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="">
+                <div className="text-left">
+                  <div class="absolute overflow-hidden w-56 h-56">
+                    <div class="absolute left-0 top-0 h-16 w-16">
+                      <div class="absolute transform -rotate-45 bg-guap text-center text-white font-semibold py-1 left-[-34px] top-[32px] w-[170px]">
+                        Save 10$ !!
+                      </div>
+                    </div>
+                  </div>
+                  <img src={Bundle} className="rounded" />
+                  <h1 className="font-bold mb-2">Bundle</h1>
+                  <p className="text-guap italic text-sm">$15.99</p>
+                  <div className="reviews flex">
+                    <p className="text-sm flex my-auto text-yellow-400">
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarFill />
+                      <BsStarHalf />
+                    </p>
+                    <p className="text-sm ml-1 text-yellow-600">5 Reviews</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        )}
+
         <div className="text-center">
           <button className="p-3 bg-white border border-guap text-guap text-sm rounded-md mt-12 hover:bg-guap hover:text-white">
             <Link to="/products">SEE ALL</Link>
